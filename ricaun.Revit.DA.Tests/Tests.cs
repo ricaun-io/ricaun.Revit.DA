7543e3a9c033177f95d7e1b8d44776671ea5dd2c
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-01-20 16:43:29 -0300

    Test addInNameContain

--- ricaun.Revit.DA.Tests/Tests.cs
+++ ricaun.Revit.DA.Tests/Tests.cs
@@ -9,9 +9,9 @@ namespace ricaun.Revit.DA.Tests
 {
     public class Tests : BundleFileTests
     {
-        [TestCase("2024", "4.7", "19")]
-        [TestCase("2025", "4.7", "19")]
-        public async Task ExecuteDesignAutomation(string engine, string frameworkName, string reference)
+        [TestCase("2024", "4.7", "19", null)]
+        [TestCase("2025", "4.7", "19", null)]
+        public async Task ExecuteDesignAutomation(string engine, string frameworkNameContain, string referenceContain, string addInNameContain)
         {
             var bundlePaths = GetBundles();
             if (!bundlePaths.Any())
@@ -23,8 +23,13 @@ namespace ricaun.Revit.DA.Tests
             Console.WriteLine(output.ToJson());
 
             Assert.IsTrue(output.VersionName.Contains(engine), $"VersionName contains engine {engine}");
-            Assert.IsTrue(output.FrameworkName.Contains(frameworkName), $"FrameworkName contains framework {frameworkName}");
-            Assert.IsTrue(output.Reference.Contains(reference), $"Reference contains reference {reference}");
+            Assert.IsTrue(output.FrameworkName.Contains(frameworkNameContain), $"FrameworkName contains framework {frameworkNameContain}");
+            Assert.IsTrue(output.Reference.Contains(referenceContain), $"Reference contains reference {referenceContain}");
+
+            if (output.AddInName is not null)
+                Assert.IsTrue(output.AddInName.Contains(addInNameContain), $"AddinName contains addin {addInNameContain}");
+            else
+                Assert.AreEqual(addInNameContain, output.AddInName);
         }
     }
 }
